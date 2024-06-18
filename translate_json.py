5b5a8b491b8b49d885b9f806ff6ba505ebfb38a4
Łukasz Sendecki <sendecki.lukasz111@gmail.com>
2024-06-18 18:41:13 +0200

    refactor: remove unused main

--- translate_json.py
+++ translate_json.py
@@ -16,22 +16,3 @@ def translate_json(input_json, src='en', dest='pl'):
         return translate_text(input_json, src, dest)
     else:
         return input_json
-
-def main(input_file, output_file):
-    with open(input_file, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-    
-    translated_data = translate_json(data)
-    
-    with open(output_file, 'w', encoding='utf-8') as f:
-        json.dump(translated_data, f, ensure_ascii=False, indent=4)
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python translate_json.py <input_file> <output_file>")
-        sys.exit(1)
-    
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
-    
-    main(input_file, output_file)
